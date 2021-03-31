29d2092b4c8c6f044595d88c996d4b747ce5747e
Andrew <37455284+manloeng@users.noreply.github.com>
2021-03-31 21:45:44 +0100

    Add getGenreList method

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { api_key, baseUrl } from "./secret.json";
 export async function getMoviesByPopulatrity(pageNumber) {
-  if (!pageNumber) return await axios.get(`${baseUrl}/popular?api_key=${api_key}`);
-  return await axios.get(`${baseUrl}/popular?page=${pageNumber}&api_key=${api_key}`);
+  if (!pageNumber) return await axios.get(`${baseUrl}/movie/popular?api_key=${api_key}`);
+  return await axios.get(`${baseUrl}/movie/popular?page=${pageNumber}&api_key=${api_key}`);
+}
+export async function getGenreList() {
+  return await axios.get(`${baseUrl}/genre/movie/list?api_key=${api_key}`);
 }
