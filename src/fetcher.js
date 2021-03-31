43333d29961e23d20fac25f089b425e2da5ca901
Andrew <37455284+manloeng@users.noreply.github.com>
2021-03-31 21:29:13 +0100

    Update: Fetch request to take page number - pagination

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
-async function getMoviesByPopulatrity() {
-  return await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.api_key}`);
+import { api_key, baseUrl } from "./secret.json";
+export async function getMoviesByPopulatrity(pageNumber) {
+  if (!pageNumber) return await axios.get(`${baseUrl}/popular?api_key=${api_key}`);
+  return await axios.get(`${baseUrl}/popular?page=${pageNumber}&api_key=${api_key}`);
 }
-
-module.export = { getMoviesByPopulatrity };
