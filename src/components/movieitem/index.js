967f3756d90f948f8a29df70f315b225ad7ea935
Andrew <37455284+manloeng@users.noreply.github.com>
2021-03-31 21:46:05 +0100

    Update Movie Item Compoent - remove warnings

--- src/components/movieitem/index.js
+++ src/components/movieitem/index.js
@@ -1,31 +1,26 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export default class MovieItem extends React.Component {
-
-  render () {
-    return (
-      // Complete the MovieItem component
-      <MovieItemWrapper>
-        <LeftCont>
-        </LeftCont>
-        <RightCont>
-        </RightCont>
-      </MovieItemWrapper>
-    )
-  }
+export default function MovieItem({ movie }) {
+  return (
+    // Complete the MovieItem component
+    <MovieItemWrapper key={movie.title}>
+      <LeftCont></LeftCont>
+      <RightCont></RightCont>
+    </MovieItemWrapper>
+  );
 }
 
 const MovieItemWrapper = styled.div`
   position: relative;
   background-color: white;
   border-radius: 3px;
-`
+`;
 
 const LeftCont = styled.div`
   display: inline-block;
-`
+`;
 
 const RightCont = styled.div`
   display: inline-block;
-`
+`;
