1fdf3d6a0a0a32bae639265312e1d359fe85821b
Andrew <37455284+manloeng@users.noreply.github.com>
2021-03-31 21:45:25 +0100

    Update: MovieList component

--- src/components/movielist/index.js
+++ src/components/movielist/index.js
@@ -1,21 +1,43 @@
-import React from "react";
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { getMoviesByPopulatrity, getGenreList } from "../../fetcher";
 
-import MovieItem from '../movieitem';
+import MovieItem from "../movieitem";
+import Checkbox from "../checkbox";
 
-export default class MovieList extends React.Component {
+export default function MovieList() {
+  const [data, setData] = useState();
+  const [genres, setGenres] = useState();
 
-  render () {
-    const { movies, genres } = this.props;
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await getMoviesByPopulatrity();
+      setData(data);
+    }
 
-    return (
-      <MoviesWrapper>
-        {/* Finish the MovieItem component and use it here to display the movie results */}
-      </MoviesWrapper>
-    )
-  }
+    async function fetchGenreList() {
+      const {
+        data: { genres },
+      } = await getGenreList();
+      setGenres(genres);
+    }
+
+    fetchData();
+    fetchGenreList();
+  }, []);
+
+  return (
+    <MoviesWrapper>
+      {data && data.results.map((movie) => <MovieItem movie={movie}></MovieItem>)}
+
+      <div>
+        {/* search bar */}
+        <Checkbox genres={genres}></Checkbox>
+      </div>
+    </MoviesWrapper>
+  );
 }
 
 const MoviesWrapper = styled.div`
   position: relative;
-`
+`;
