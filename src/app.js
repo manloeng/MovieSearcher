a5e8007050166466f02a0c5242b85d7c1e9598a0
Andrew <37455284+manloeng@users.noreply.github.com>
2021-03-31 21:58:30 +0100

    Remove: Nav padding - mobile responsiveness

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import SideNavBar from "./components/sidenavbar";
 
 import Discover from "./pages/discover";
 
-import './css/app.css'; 
+import "./css/app.css";
 
 export default class App extends React.Component {
-  render () {
+  render() {
     return (
       <Router>
         <PageContainer>
           <SideNavBar {...this.props} />
           <ContentWrapper>
             <Switch>
-              <Route path="/discover" component={Discover} {...this.props}/>
+              <Route path="/discover" component={Discover} {...this.props} />
             </Switch>
           </ContentWrapper>
         </PageContainer>
@@ -25,11 +25,14 @@ export default class App extends React.Component {
   }
 }
 
-
 const ContentWrapper = styled.main`
   padding-left: 280px;
-`
+
+  @media screen and (max-width: 768px) {
+    padding-left: 0;
+  }
+`;
 
 const PageContainer = styled.main`
   overflow-x: hidden;
-`
+`;
