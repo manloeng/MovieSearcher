537835b83bb0060e64b37d9940730a2158d0ccb8
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-06 21:05:34 +0100

    Update SearchBar Color

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// import * as colors from "../../colors";
+import * as colors from "../../colors";
 // import SearchIcon from "../../images/search-icon-yellow.png";
 // import CalendarIcon from "../../images/year-icon.png";
 
 export default function SearchBar({ handleChange, name }) {
   return (
-    <div className="App">
-      <input type="text" value={name} onChange={handleChange} />
-    </div>
+    <SearchBarStyle type="text" value={name} onChange={handleChange} placeholder="Search for Movies"></SearchBarStyle>
   );
 }
+
+const SearchBarStyle = styled.input`
+  border: none;
+  border-bottom: 1px solid ${colors.primaryColor};
+  color: ${colors.primaryColor};
+`;
